eab5c2da5cff18e44138e3c3ac3687592bc6e2b5
Axel <00088119@uca.edu.sv>
2019-09-13 19:38:59 -0600

    actividades

--- Laboratorio02.cpp
+++ Laboratorio02.cpp
@@ -19,7 +19,7 @@ int conteo(int n){
     return n < 10 ? 1 : 1 + conteo(n/10);
 
 }*/
-int Euclides(int mayor, int menor, int mcd);
+/*int Euclides(int mayor, int menor, int mcd);
 int main(void){
     int mayor = 0, menor = 0;
     cout << "ingrese numero mayor: ";cin >> mayor;
@@ -46,4 +46,30 @@ int Euclides(int mayor, int menor, int mcd){
         return Euclides(mayor, menor, mcd);
 
     }
+}*/
+void hanoi(int num,char A,char C,char B)
+{
+    if(num==1)
+    {
+        cout<<"Mueva el bloque "<<num<<" desde "<<A<<" hasta  "<<C<<endl;
+
+    }
+    else
+    {
+        hanoi(num-1,A,B,C);
+        cout<<"Mueva el bloque "<<num<<" desde "<<A<<" hasta  "<<C<<endl;
+        hanoi(num-1,B,C,A);
+    }
+}
+
+int main()
+{
+    int n;
+    char A,B,C;
+
+    cout<<"Los clavijas son A B C"<<endl;
+    cout<<"Numero de discos: ";
+    cin>>n;
+    hanoi(n,'A','C','B');
+
 }
