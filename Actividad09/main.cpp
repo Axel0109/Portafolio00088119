5aa01ae64869c30b278da22768fe58103645a2d9
Axel <00088119@uca.edu.sv>
2019-09-19 12:40:58 -0600

    ejercicios

---
+++ Actividad09/main.cpp
@@ -0,0 +1,71 @@
+//de la actividad 7
+#include <iostream>
+#include <stack>
+using namespace std;
+
+//primer punto
+/*int main()
+{
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    s.pop();
+    cout << "el segundo elemento es: " << s.top() << endl;
+    s.pop();
+
+
+    if (s.empty())
+        cout << "lista vacia\n";
+
+
+    return 0;
+}*/
+/*int main()
+{
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    int e = s.top();
+    s.pop();
+    cout << "el segundo elemento es: " << s.top() << endl;
+    s.push(e);
+
+
+
+    if (s.empty())
+        cout << "pila vacia\n";
+
+
+    return 0;
+}*/
+//punto 3
+/*int main()
+{
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    s.pop();
+    s.pop();
+    s.pop();
+    cout << "el ultimo elemento es: " << s.top() << endl;
+    s.pop();
+
+
+    if (s.empty())
+        cout << "pila vacia\n";
+
+
+    return 0;
+}*/
